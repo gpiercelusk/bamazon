77dd6c90b615fb398d9b2ca8173728b79a96b7a2
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-04-30 21:45:13 -0400

    finished add to inventory functionality

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var lineBreak = "--------------------------------------------\n"
+var lineBreak = "\n--------------------------------------------\n"
 
 
 var connection = mysql.createConnection({
@@ -27,8 +27,8 @@ function managerUser() {
       choices: [
         "View Items for Sale",
         "View Low Stock",
-        "Add Stock",
-        "Add to Inventory"
+        "Receive Inventory",
+        "Add New Item to Inventory"
       ]
     }
   ]).then(function (res) {
@@ -38,12 +38,12 @@ function managerUser() {
     } else if (res.manager === "View Low Stock") {
       console.log(lineBreak);
       readLowStock();
-    } else if (res.manager === "Add Stock") {
+    } else if (res.manager === "Receive Inventory") {
       console.log(lineBreak);
       addStock();
-    } else if (res.manager === "Add to Inventory") {
+    } else if (res.manager === "Add New Item to Inventory") {
       console.log(lineBreak);
-      addInventory();
+      addNew();
     }
   });
 }
@@ -71,9 +71,52 @@ function readLowStock() {
 }
 
 function addStock() {
-
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "chooseId",
+      message: "Select item you would like to add to stock.(Select with ID)"
+    },
+    {
+      type: "input",
+      name: "howMany",
+      message: "How many would came in?"
+    }
+  ]).then(function (res) {
+    checkId(res.chooseId, res.howMany)
+  });
 }
 
-function addInventory() {
+function checkId(id, units) {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    if (res[id - 1] === undefined) {
+      console.log("Item not a product. Plese select a valid option");
+      addStock();
+    } else {
+      updateItem(id, res[id - 1].stock_quantity, units)
+    }
+  });
+}
+
+function updateItem(id, initialUnits, units) {
+  connection.query('UPDATE products SET ? WHERE ?',
+    [
+      {
+        stock_quantity: parseInt(initialUnits) + parseInt(units)
+      },
+      {
+        id: id
+      }
+    ],
+    function (err) {
+      if (err) throw err
+      console.log(lineBreak + "\nProduct inventory updated. Product ID #" + id + " increased inventory by " + units + " units.");
+      console.log(lineBreak);
+      managerUser();
+    });
+}
+
+function addNew() {
 
 }
